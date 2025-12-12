ccc3798a2f0acd96b1307b49be11f893ba9363b0
xd-sarthak <sarthak.sri02032gmail.com>
2025-12-12 18:53:51 +0530

    init

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button>Hello World</Button>
+    </div>
+  );
+}
