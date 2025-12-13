51c554ab05ca01feffa4cd140187022d92a4c407
xd-sarthak <sarthak.sri02032gmail.com>
2025-12-13 09:35:57 +0530

    logout button added

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import { requireAuth } from "@/module/auth/utils/auth-utils";
+import Logout from "@/module/auth/components/logout";
 export default async function Home() {
   await requireAuth()
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Hello World</Button>
+      <Logout>
+          <Button>Logout</Button>
+      </Logout>
     </div>
   );
 }
--- my-app/module/auth/components/logout.tsx
+++ my-app/module/auth/components/logout.tsx
@@ -1,8 +1,25 @@
+"use client"
 import React from 'react'
-
-const Logout = () => {
+import { signOut } from '@/lib/auth-client'
+import { useRouter } from 'next/navigation'
+const Logout = ({
+  children,
+  className
+}:{
+  children:React.ReactNode,
+  className?:string
+}) => {
+  const router = useRouter()
   return (
-    <div>L</div>
+    <span className={className} onClick={() => signOut({
+      fetchOptions:{
+        onSuccess:() => {
+          router.push("/login")
+        }
+      }
+    })}>
+      {children}
+    </span>
   )
 }
 
