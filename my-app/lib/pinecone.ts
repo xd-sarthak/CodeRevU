337ccb3b0609a74962e0855881145ecedc72e305
xd-sarthak <sarthak.sri02032gmail.com>
2025-12-16 17:43:54 +0530

    pinecone,inngest and AI-SDK initialised

---
+++ my-app/lib/pinecone.ts
@@ -0,0 +1,8 @@
+import {Pinecone} from "@pinecone-database/pinecone";
+
+export const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!
+    
+}); 
+
+export const pineconeIndex = pinecone.Index("coderevu-vectorembeddings-v1");
